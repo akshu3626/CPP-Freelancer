d0fb7da9a1964ed8040869d2a7e9730954a25681
akshay41 <akshujoshi41@gmail.com>
2023-04-26 18:53:20 +0100

    initial commit

---
+++ account/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    is_client = "Client"
+    is_freelancer = "Freelancer"
+    
+    ROLE = [ (is_client, "Client"),(is_freelancer, "Freelancer"),]
+    role = models.CharField(max_length=50,choices=ROLE,default="is_client", null=True )
+    
